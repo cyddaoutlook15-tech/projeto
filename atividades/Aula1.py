60e6ac059898330eababd46b4001335dee50ad17
cyddaoutlook15-tech <cyddaoutlook15@gmail.com>
2026-03-13 19:14:17 -0300

    Create Aula1.py

---
+++ atividades/Aula1.py
@@ -0,0 +1,64 @@
+#Exercício 1
+
+"""
+a = 10
+b = 3.5
+c = "Python"
+d = True
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+"""
+
+#Exercício 2
+"""
+idade = 25
+print("Eu tenho", idade, "anos.")
+"""
+
+#Exercício 3
+"""
+n1= int(input("Digite o primeiro número:"))
+n2= int(input("Digite o segundo número:"))
+soma = n1 + n2
+print("A soma é:", soma)
+"""
+
+#Exercício 4
+
+"""
+numero = 15.89
+inteiro= int(numero)
+print("O número inteiro é:", inteiro)
+"""
+
+#Exercício 5
+"""
+x = 10 
+y = 5
+
+resultado = x > y
+print("O resultado da comparação é:", resultado)
+"""
+
+#Exercício 6
+
+"""
+nome = input("Digite seu nome: ")
+idade = int(input("Digite sua idade: "))
+altura = float(input("Digite sua altura: "))
+
+print(f"Olá, {nome}! Você tem {idade} anos e sua altura é {altura} metros.")
+"""
+
+#Exercício 7
+"""
+numero_texto = "50"
+n1 = int(numero_texto) * 2
+print(n1)
+
+n2= int(numero_texto) * 2
+print(n2)
+"""
