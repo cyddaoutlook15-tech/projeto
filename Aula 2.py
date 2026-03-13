2681cdd06b885de4cb532b5101b9d6b7e0585c12
cyddaoutlook15-tech <cyddaoutlook15@gmail.com>
2026-03-13 19:16:22 -0300

    Create Aula 2.py

---
+++ Aula 2.py
@@ -0,0 +1,70 @@
+#Exercício 1
+"""
+num = int (input("Digite um número inteiro: "))
+antecessor = num - 1
+sucessor = num + 1  
+print("O antecessor de", num, "é", antecessor)
+print("O sucessor de", num, "é", sucessor)
+"""
+
+#Exercício 2
+"""
+n1 = float (input("Digite o primeiro número: "))
+n2 = float (input("Digite o segundo número: "))
+media = (n1 + n2) / 2
+
+print("A média entre", n1, "e", n2, "é", media)
+"""
+
+#Exercício 3
+"""
+num = float (input("Digite um número: "))
+dobro = num * 2
+triplo = num * 3
+raiz = num ** 0.5
+
+print("O dobro de", num, "é", dobro)
+print("O triplo de", num, "é", triplo)
+print("A raiz quadrada de", num, "é", raiz)
+"""
+
+#Exercício 4
+
+"""
+balas = 50
+crianças = 6
+
+balas_por_criança = balas // crianças
+balas_restantes = balas % crianças
+
+print("Cada criança receberá", balas_por_criança, "balas.")
+print("Sobrarão", balas_restantes, "balas.")
+"""
+
+
+#Exercício 5
+
+"""
+dolar = 5.80
+reais = float (input("Quantos reais você tem na carteira(R$)? "))
+
+resultado = reais / dolar
+print("Com R$", reais, "você pode comprar US$", resultado)
+"""
+
+#Exercício 6
+"""
+idade = int (input("Digite a sua idade: "))
+convite = input("Você tem convite para a festa? (True/False) ")
+
+resultado = idade > 18 and convite == "True"
+print(resultado)
+"""
+
+#Exercício 7
+"""
+preço = float (input("Digite o preço do produto: R$ "))
+novo_preço = preço - (preço * 15/100)
+
+print("O novo preço do produto com 15% de desconto é: R$", novo_preço)
+"""
